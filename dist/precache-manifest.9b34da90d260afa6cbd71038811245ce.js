09a493a60ecfb1ec91ebe3751e2ce1d4be5213e2
hdjerry <damilolajerryhd@gmail.com>
2019-07-21 17:14:41 +0100

    Adding files.

---
+++ dist/precache-manifest.9b34da90d260afa6cbd71038811245ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ecaa0ebf60d04de5352",
+    "url": "/js/about.96c316d1.js"
+  },
+  {
+    "revision": "59d5d5b46b1d0bead502",
+    "url": "/css/app.6fe9e53f.css"
+  },
+  {
+    "revision": "59d5d5b46b1d0bead502",
+    "url": "/js/app.0746f179.js"
+  },
+  {
+    "revision": "bb76cdcf053362243156",
+    "url": "/js/chunk-vendors.40dfe92e.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ad920bf3dd1f96d74bcb794816269474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
